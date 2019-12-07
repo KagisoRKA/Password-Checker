5f32f8b136c163c1c01ae93b39aa3784cf1bf51a
KagisoRKA <57135740+KagisoRKA@users.noreply.github.com>
2019-12-07 19:47:49 +0200

    Deleting Main.java (will replace it with a complete gradle project)

--- PasswordChecker/Main.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class Main {
-    public static void main(String[] args) {
-        Scanner Obj = new Scanner(System.in);
-        System.out.println("Set your password: ");
-        String password = Obj.nextLine();
-        System.out.println("Password is ok?: " +PasswordChecker.passwordIsOk(password));
-    }
-}
